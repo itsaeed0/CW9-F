6854611b75a7dd1e2d65b819522053ef20e0d8f5
itsaeed0 <ashourisaeid78@gmail.com>
2024-10-19 23:09:46 +0330

    saeed

--- q1/app.js
+++ q1/app.js
@@ -1,24 +1,21 @@
-
 function myFunction() {
-    let element = document.body;
-    element.classList.toggle("dark");
-  
-    if (element.classList.contains("dark")) {
-      localStorage.setItem("theme", "dark");
-    } else {
-      localStorage.setItem("theme",   
-   "light");
-    }
+  let element = document.body.classList.toggle("dark");
+
+  if (element.classList.contains("dark")) {
+    localStorage.setItem("theme", "dark");
+  } else {
+    localStorage.setItem("theme", "light");
   }
-  
-  let put = document.getElementById("changeThem");
-  put.addEventListener('click', () => myFunction());
-  
-  window.addEventListener("DOMContentLoaded", () => {
-    let theme = localStorage.getItem("theme");
-    if (theme === "dark") {
-      document.body.classList.add("dark");
-    } else {
-      document.body.classList.remove("dark");
-    }
-  });
+}
+
+let put = document.getElementById("changeThem");
+put.addEventListener("click", () => myFunction());
+
+window.addEventListener("DOMContentLoaded", () => {
+  let theme = localStorage.getItem("theme");
+  if (theme === "dark") {
+    document.body.classList.add("dark");
+  } else {
+    document.body.classList.remove("dark");
+  }
+});
